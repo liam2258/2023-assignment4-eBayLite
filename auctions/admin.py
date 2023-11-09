8c0c09e7bf5705cc874428523c68a8a109bd976e
Liam <helm_of_protection@hotmail.com>
2023-11-08 20:49:28 -0900

    test change

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User, Category, Listing, Comment, Bid
 
+# Small change
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
