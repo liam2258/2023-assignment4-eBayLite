cfe83775ef4c21bd04ae9c29c2da1e8be58fb8ab
Liam <helm_of_protection@hotmail.com>
2023-11-18 22:32:29 -0900

    newstuff

--- auctions/views.py
+++ auctions/views.py
@@ -126,7 +126,7 @@ def displayWatchList(request):
         "listings": listings
     })
 
-'''def removeWatchList(request, id):
+def removeWatchList(request, id):
     listingData = Listing.objects.get(pk=id)
     currentUser = request.user
     listingData.watchlist.remove(currentUser)
@@ -136,24 +136,24 @@ def addWatchList(request, id):
     listingData = Listing.objects.get(pk=id)
     currentUser = request.user
     listingData.watchlist.add(currentUser)
-    return HttpResponseRedirect(reverse("listing", args=(id, )))'''
-def removeWatchList(request, id):
-    try:
-        listingData = Listing.objects.get(pk=id)
-        currentUser = request.user
-        listingData.watchlist.remove(currentUser)
-        return JsonResponse({'success': True})
-    except Listing.DoesNotExist:
-        return JsonResponse({'success': False, 'error': 'Listing does not exist'}, status=404)
+    return HttpResponseRedirect(reverse("listing", args=(id, )))
+# def removeWatchList(request, id):
+#     try:
+#         listingData = Listing.objects.get(pk=id)
+#         currentUser = request.user
+#         listingData.watchlist.remove(currentUser)
+#         return JsonResponse({'success': True})
+#     except Listing.DoesNotExist:
+#         return JsonResponse({'success': False, 'error': 'Listing does not exist'}, status=404)
 
-def addWatchList(request, id):
-    try:
-        listingData = Listing.objects.get(pk=id)
-        currentUser = request.user
-        listingData.watchlist.add(currentUser)
-        return JsonResponse({'success': True})
-    except Listing.DoesNotExist:
-        return JsonResponse({'success': False, 'error': 'Listing does not exist'}, status=404)
+# def addWatchList(request, id):
+#     try:
+#         listingData = Listing.objects.get(pk=id)
+#         currentUser = request.user
+#         listingData.watchlist.add(currentUser)
+#         return JsonResponse({'success': True})
+#     except Listing.DoesNotExist:
+#         return JsonResponse({'success': False, 'error': 'Listing does not exist'}, status=404)
     
 def index(request):
     activeListings = Listing.objects.filter(isActive=True)
